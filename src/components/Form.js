2d7fc5898f89c08934c5e320a9e198e05b48efa7
Sandipgaikwad1598 <sandipgaikwad1598@gmail.com>
2022-07-22 20:38:03 +0530

    second

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,73 +1,157 @@
-import Header from "./Header"
-import { useNavigate } from "react-router-dom"
-import axios from "axios";
-import { useState } from "react"
-import Moment from 'moment';
-const address="https://instaclone-server-10.herokuapp.com/post";
-// const heroku="https://insta-relica-server.addressapp.com/post"
+// import Header from "./Header"
+// import { useNavigate } from "react-router-dom"
+// import axios from "axios";
+// import { useState } from "react"
+// import Moment from 'moment';
+// const address="https://instaclone-server-10.herokuapp.com/post";
+// // const heroku="https://insta-relica-server.addressapp.com/post"
 
-const Form=()=>{
-    const date = Moment().format("DD MMM YYYY");
-    const [data,setdata]=useState({})
-    const [image,setimage]=useState('')
-    const navigate =useNavigate()
-    const hover={
-        backgroundColor:"#2d92d1",
-        color:"white",
-        height:"1.5rem",
-        border: "none"
-    }
-    const handlesubmit=(e)=>{
-        e.preventDefault()
-        let formData=new FormData()
-        formData.append('testimage',image)
-        formData.append('name',data.author)
-        formData.append('location',data.location)
+// const Form=()=>{
+//     // const date = Moment().format("DD MMM YYYY");
+//     const [data,setdata]=useState({})
+//     const [image,setimage]=useState('')
+//     const navigate =useNavigate()
+//     const hover={
+//         backgroundColor:"#2d92d1",
+//         color:"white",
+//         height:"1.5rem",
+//         border: "none"
+//     }
+//     const handlesubmit=(e)=>{
+//         e.preventDefault()
+//         let formData=new FormData()
+//         formData.append('testimage',image)
+//         formData.append('name',data.author)
+//         formData.append('location',data.location)
 
         
-        formData.append('description',data.description)
-        formData.append('date',date)
-        axios.post(address,formData,{headers:{"Content-type":"multipart/form-data"},}).then((res)=>{
-            console.log("success")
-            navigate("/postview")
-        }).catch((err)=>{
-            console.log(err)
-        })
+//         formData.append('description',data.description)
+//         formData.append('date',date)
+//         axios.post(address,formData,{headers:{"Content-type":"multipart/form-data"},}).then((res)=>{
+//             console.log("success")
+//             navigate("/postview")
+//         }).catch((err)=>{
+//             console.log(err)
+//         })
+//     }
+//     function SubmitButton(){
+//         if (Object.keys(data).length===3 && image){
+//           return (
+//             <div className="buttonbody">
+//             <input className="submitbutton" type={"button"} value='Post' style={hover} onClick={handlesubmit}/>
+//             </div>
+//           )
+//         } else {
+//           return (
+//             <div className="buttonbody">
+//                 <button className="submitbutton" type="button" disabled>Post</button>
+//             </div>
+//           )
+//         };
+//       };
+//     return(
+//         <>
+//         <Header/>
+//         <div className="formtocenter">
+//         <form className="formbody">
+//         <span>
+//             <input type={"file"} name="testimage" onChange={(e)=>{setimage(e.target.files[0])}}/>
+//         </span>
+//             <div className="nameloc">
+//                 <input type={"text"} className='authloc' placeholder="Author" onChange={(e)=>{setdata({...data, author: e.target.value})}} />
+//                 <input type={"text"} className='authloc' placeholder="Location" onChange={(e)=>{setdata({...data, location: e.target.value})}} />
+//             </div>
+//             <input type={"text"} placeholder="Description" onChange={(e)=> {setdata({...data, description: e.target.value})}} />
+//             <br/>
+//         <SubmitButton/>  
+//         </form>
+//         </div>
+
+//         </>
+//     )
+// }
+// export default Form;
+
+
+import Logo from "../assets/logo1.png";
+import Camera from "../assets/cam2.jpg"
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+import Filebase64 from "react-file-base64"
+import axios from "axios";
+import './form.css'
+
+const Form_page=() =>{
+    const navigate = useNavigate()
+    const [data,setdata] = useState({author: "", location:"", image:"", description:""})
+    const [image,setimage]=useState('')
+    
+    const inputhadler = (id)=> {
+        setdata({...data,image:id})
     }
-    function SubmitButton(){
-        if (Object.keys(data).length===3 && image){
-          return (
-            <div className="buttonbody">
-            <input className="submitbutton" type={"button"} value='Post' style={hover} onClick={handlesubmit}/>
-            </div>
-          )
-        } else {
-          return (
-            <div className="buttonbody">
-                <button className="submitbutton" type="button" disabled>Post</button>
-            </div>
-          )
-        };
-      };
+    
+    const handlesubmit=(e)=>{
+        e.preventDefault()
+        axios({
+            url:"https://instaclone-server-10.herokuapp.com/uploads",
+            method:"POST",
+            headers :{
+            },
+            data : data
+           }).then((post)=>{
+                navigate("/postview")
+           }).catch((err)=>{
+            console.log(err)
+           })
+           console.log(data)
+           setdata({author:"",location:"",description:"",image : ""})
+    }
+    
+    
     return(
         <>
-        <Header/>
-        <div className="formtocenter">
-        <form className="formbody">
-        <span>
-            <input type={"file"} name="testimage" onChange={(e)=>{setimage(e.target.files[0])}}/>
-        </span>
-            <div className="nameloc">
-                <input type={"text"} className='authloc' placeholder="Author" onChange={(e)=>{setdata({...data, author: e.target.value})}} />
-                <input type={"text"} className='authloc' placeholder="Location" onChange={(e)=>{setdata({...data, location: e.target.value})}} />
-            </div>
-            <input type={"text"} placeholder="Description" onChange={(e)=> {setdata({...data, description: e.target.value})}} />
-            <br/>
-        <SubmitButton/>  
-        </form>
-        </div>
+         <header>
+            <div className="nav">
+                <img src={Logo} alt="insta-logo"></img>
+                <img id="camera" src={Camera} alt="camera"></img>
 
+            </div>
+        </header>
+        <hr />
+        
+        <form onSubmit={handlesubmit}>
+            <div className='form-box'>
+            <h2>ADD POST</h2>
+            <hr/>
+           
+            <Filebase64
+                     type="file"
+                     multiple={false}
+                     onDone = {({base64})=> inputhadler(base64)}
+                     id="file"/>
+            
+           
+            <div className="part-2">
+                <div>
+                <input type="text"   placeholder="Author" onChange={(e)=>{setdata({...data, author: e.target.value})}} />
+                <input type="text"  class="inline"  placeholder="Location" onChange={(e)=>{setdata({...data, location: e.target.value})}}/>
+                </div>
+                
+            </div>
+            <div className='part-3'>
+                <input id="descrip" type="text"  placeholder="Description" onChange={(e)=> {setdata({...data, description: e.target.value})}}/>
+            </div>
+            <div className='part-4'>
+                <button type="submit" id="b2"> Post</button>
+          
+            </div>
+                
+            </div>
+            
+        </form>
         </>
     )
 }
-export default Form;
+export default Form_page;
+
--- src/components/Postview.js
+++ src/components/Postview.js
@@ -1,59 +1,133 @@
-import Header from "./Header"
-import axios from "axios"
-import { useState,useEffect } from "react"
-const address="https://instaclone-server-10.herokuapp.com/";
-// const heroku="https://insta-relica-server.herokuapp.com/";
+// import Header from "./Header"
+// import axios from "axios"
+// import { useState,useEffect } from "react"
+// const address="https://instaclone-server-10.herokuapp.com/images";
+// // const heroku="https://insta-relica-server.herokuapp.com/";
 
-const Postview =()=>{
+// const Postview =()=>{
+//     const [data,setpost]=useState([])
+//     useEffect(()=>{
+//         axios.get(address)
+//         .then((res)=>{setpost(res.data)})
+//         .catch((err)=>{console.log(err)})
+//     },[])
+//     return(
+//         <>
+//         <Header/>
+//         {
+//             data.map((user,i)=>{
+//                 const base64str=btoa(
+//                     new Uint8Array(user.image.data.data).reduce(function(data,byte){
+//                         return data+String.fromCharCode(byte)
+//                     },"")
+//                 );
+//                 return (
+//                     <div className='content' key={i}>
+//                     <div >
+//                         <header id='head'>
+//                           <div  >
+//                             <h3 className='name_loc'>{user.author}</h3>
+//                             <h4 className='name_loc' style={{fontWeight:'100'}} >{user.location}</h4>
+//                           </div>
+//                             <h2 style={{margin:'0px'}}>...</h2>
+//                         </header>
+//                     </div>
+//                     <div>
+//                     <img className='image' src={`data:image/png;base64,${base64str}`} alt={"err"} />
+//                     </div>
+//                     <div id='footer'>
+//                       <div id='footer_flex'>
+//                         <div >
+//                           <img className='like_share' src='https://p.kindpng.com/picc/s/169-1694281_heart-symbol-computer-icons-heart-icon-instagram-png.png' alt=''></img>
+//                           <img className='like_share' src='https://cdn0.iconfinder.com/data/icons/simple-line-design/100/Fast_delivery-512.png' alt=''></img>
+//                         </div>
+//                         <h6 style={{margin:'1px'}} className='date'>{user.date}</h6>
+//                       </div>
+//                       <h6 style={{margin:'10px 4px'}} className='likes'>{user.like} likes</h6>
+//                       <h4 style={{margin:'10px 4px'}}>{user.description}</h4>
+//                     </div>
+//                     </div>
+//                 )
+
+
+//             })
+//         }
+        
+//         </>
+//     )
+// }
+// export default Postview
+
+
+import Logo from "../assets/logo1.png";
+import Camera from "../assets/cam2.jpg"
+import { useEffect, useState } from "react";
+
+import axios from "axios"
+import "./post_view.css";
+
+
+const PostView = () => {
     const [data,setpost]=useState([])
     useEffect(()=>{
-        axios.get(address)
-        .then((res)=>{setpost(res.data)})
+        axios.get("https://instaclone-server-10.herokuapp.com/images").then((imagedata)=>{
+            let data = imagedata.data.images.reverse();
+            //console.log(data[0])
+            setpost(data)
+            
+        })
         .catch((err)=>{console.log(err)})
     },[])
-    return(
+    
+    return (
         <>
-        <Header/>
-        {
-            data.map((user,i)=>{
-                const base64str=btoa(
-                    new Uint8Array(user.image.data.data).reduce(function(data,byte){
-                        return data+String.fromCharCode(byte)
-                    },"")
-                );
-                return (
-                    <div className='content' key={i}>
-                    <div >
-                        <header id='head'>
-                          <div  >
-                            <h3 className='name_loc'>{user.author}</h3>
-                            <h4 className='name_loc' style={{fontWeight:'100'}} >{user.location}</h4>
-                          </div>
-                            <h2 style={{margin:'0px'}}>...</h2>
-                        </header>
-                    </div>
-                    <div>
-                    <img className='image' src={`data:image/png;base64,${base64str}`} alt={"err"} />
-                    </div>
-                    <div id='footer'>
-                      <div id='footer_flex'>
-                        <div >
-                          <img className='like_share' src='https://p.kindpng.com/picc/s/169-1694281_heart-symbol-computer-icons-heart-icon-instagram-png.png' alt=''></img>
-                          <img className='like_share' src='https://cdn0.iconfinder.com/data/icons/simple-line-design/100/Fast_delivery-512.png' alt=''></img>
-                        </div>
-                        <h6 style={{margin:'1px'}} className='date'>{user.date}</h6>
-                      </div>
-                      <h6 style={{margin:'10px 4px'}} className='likes'>{user.like} likes</h6>
-                      <h4 style={{margin:'10px 4px'}}>{user.description}</h4>
-                    </div>
-                    </div>
-                )
+       
+        <div className="container">
+                <header>
+                    <div className="nav">
+                        <img src={Logo} alt="insta-logo"></img>
+                        <a href="./form"><img id="camera" src={Camera} alt="camera"></img></a>
+                        
 
-
-            })
-        }
-        
+                    </div>
+                </header>
+                <hr />
+                <div>
+                    {data.map((user,i)=>{
+                         return (
+                                <div className="body">
+                                    <div className="card">
+                                        <div className="user-info" key={i}>
+                                            <h3>{user.author}</h3>
+                                            <span className="location">{user.location}</span>
+                                            <span className="dot"><h1>...</h1></span>
+                                        </div>
+                                        <div className="user-image">
+                                            <img className="post" src={user.image} alt="user defined "></img>
+                                        </div>
+                                        <div className="cont1">
+                                        <div className="heart">
+                                            <img  id="heart-image" src="/heart.png" alt="user defined"></img>
+                                            <img  id="share-image" src="/share.png" alt="user defined"></img>
+                                            {/* <span className="grid" id="date" >{user.date}</span> */}
+                                        </div>
+                                        <div className="user-meta">
+                                            <span>{user.date}</span>
+                                        </div>
+                                        </div>
+                                        <div className="user-likes">
+                                            {user.likes} likes
+                                        </div>
+                                        <div className="user-description">
+                                            {user.description}
+                                        </div>
+                                    </div>
+                                </div>)
+                        })
+                    }
+                </div>
+            </div>
         </>
     )
 }
-export default Postview
+export default PostView;
